9ac86d183a61df8911709885cca65141f7931829
jozelles <jozelle@berkeley.edu>
2025-11-09 18:50:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoiam96ZWxsZXMiLCJhIjoiY21oYmd6ZjY1MHhzMDJpcHlxa
 const map = new mapboxgl.Map({
   container: 'map', // this is the container ID that we set in the HTML
 style: 'mapbox://styles/jozelles/cmhbh45fg001m01r67ni57kig', // Your Style URL goes here
-  center: [-122.27, 37.8], // starting position [lng, lat]. Note that lat must be set between -90 and 90. You can choose what you'd like.
-  zoom: 9 // starting zoom, again you can choose the level you'd like.
+  center: [-122.25, 37.87], // starting position [lng, lat]. Note that lat must be set between -90 and 90. You can choose what you'd like.
+  zoom: 12.5 // starting zoom, again you can choose the level you'd like.
     });
 
 map.on('load', function() {
